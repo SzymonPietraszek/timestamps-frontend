7e91412be6ff58266c8a933442f6d0c1430caf4d
SzymonPietraszek <SzymonPietraszek1997@gmail.com>
2026-03-14 09:18:18 +0100

    Add timestamps endpoint

---
+++ functions/timestamps.js
@@ -0,0 +1,28 @@
+import { createSignedFetcher } from 'aws-sigv4-fetch';
+
+export const onRequest = async (context) => {
+    console.log({
+    url: context.request.url,
+    method: context.request.method,
+    });
+
+    const signedFetch = createSignedFetcher({
+        service: 'lambda',
+        region: 'eu-central-1',
+        credentials: {
+            accessKeyId: context.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: context.env.AWS_ACCESS_KEY_SECRET,
+        }
+    });
+
+    const incoming = new URL(context.request.url);
+    const backendUrl = new URL(incoming.pathname + incoming.search, context.env.BACKEND_URL);
+
+    const response = await signedFetch(backendUrl, {
+        method: context.request.method,
+    });
+
+    console.log("status:", response.status);
+
+    return response;
+};
